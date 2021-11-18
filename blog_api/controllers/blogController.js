59330bb3a58182ce6a76ba1026b7ed22e3a23d5e
sahaorchid <70687710+sahaorchid@users.noreply.github.com>
2021-11-18 17:42:12 +0530

    Update blogController.js

--- blog_api/controllers/blogController.js
+++ blog_api/controllers/blogController.js
@@ -1,6 +1,7 @@
 const blogModel = require('../models/blogModel')
 const utils = require('../utils')
 
+
 async function getBlogs(req,res){
     const blogs = await blogModel.fetchAll()
     res.writeHead(200,{'Content-Type' : 'application/json'})
@@ -10,8 +11,11 @@ async function getBlogs(req,res){
 async function addBlog(req,res){
     const data = await utils.getReqData(req)
     const { name, description, img_url } = JSON.parse(data)
-    const blog ={name,description,img_url}
-    const msg = await blogModel.addBlogData(blog)
+    const blogId = await blogModel.fetchLast()
+        let id=(parseInt(blogId)+1).toString()
+        const blog ={id,name,description,img_url}
+        msg = await blogModel.addBlogData(blog)
+    // const blog ={name,description,img_url}
     res.writeHead(200,{'Content-Type' : 'application/json'})
     res.end(JSON.stringify(msg))
 }
